6a4c3679e9868a07748ff90bec4865590c4f9e64
Lacorne Ludovic <lacorne.ludovic@gmail.com>
2026-01-29 19:00:17 +0100

    Docker compose and init mongodb

---
+++ src/config.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Generals
+DB_NAME=os.getenv('DB_NAME')
+
+DB_ROOT_USER=os.getenv('DB_ROOT_USER')
+DB_ROOT_PASSWORD=os.getenv('DB_ROOT_PASSWORD')
+
+DB_BOT_USER=os.getenv('DB_BOT_USER')
+DB_BOT_PASSWORD=os.getenv('DB_BOT_PASSWORD')
+
+# Mongo
+MONGO_DB_PORT=os.getenv('MONGO_DB_PORT')
