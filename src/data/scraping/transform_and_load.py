244989524290d66141c3ef8de7ef6d4ce64da3f7
Lacorne Ludovic <lacorne.ludovic@gmail.com>
2026-03-10 11:37:27 +0100

    Add mapping file between symbol and name

---
+++ src/data/scraping/transform_and_load.py
@@ -0,0 +1,90 @@
+import json
+import time
+
+from pathlib import Path
+from typing import cast
+
+from binance.client import Client
+from binance.exceptions import BinanceAPIException, BinanceRequestException
+
+from src.config import BINANCE_API_KEY, BINANCE_API_SECRET, PROJECT_ROOT
+from src.data.scraping.custom_logger import logger
+
+# Define a default symbols list in case of api error, or file access errors.
+DEFAULT_SYMBOLS = [
+    {
+        "symbol": "BTC",
+        "name": "Bitcoin",
+        "alisases": ["btc", "bitcoin"]
+    }
+]
+
+def get_cryptos_symbols_and_names(force_update=False) -> list[dict[str, str]] | None:
+    file_name = "mapping_cryptos_symbol_name.json"
+    path_to_symbols_and_names = Path(f"{PROJECT_ROOT}/data/external/{file_name}")
+
+    # If file exist, return the data, except when force_update is set to True
+    if not force_update and path_to_symbols_and_names.exists():
+        try:
+            with open(path_to_symbols_and_names, "r", encoding="utf-8") as f:
+                return cast(list[dict[str, str]], json.load(f))
+        except json.JSONDecodeError as e:
+            logger.error(f"Erreur lors de la lecture du JSON: {e.msg}")
+            logger.warning(f"Le fichier va être remplacé par un nouveau")
+
+    # Call binance api if force_update is set to True, or if file doesn't exist.
+    logger.info(f"Appel api afin de générer le fichier de mapping entre un symbole crypto et son nom")
+    try:
+        mapping_cryptos=list()
+        binance_client = Client(api_key=BINANCE_API_KEY, api_secret=BINANCE_API_SECRET)
+        server_time = binance_client.get_server_time()
+        binance_client.timestamp_offset = server_time['serverTime'] - int(time.time() * 1000)
+        coins_list=binance_client._request('get', f'{binance_client.MARGIN_API_URL}/v1/capital/config/getall', signed=True, data={})
+        
+        # Keep only used attributes
+        for coin in coins_list:
+            mapping_cryptos.append({
+                "symbol": coin['coin'],
+                "name": coin['name'],
+                "aliases": [str.lower(coin['coin']), str.lower(coin['name'])]
+            })
+        
+        # Save file
+        try:
+            with open(path_to_symbols_and_names, "w", encoding="utf-8") as f:
+                json.dump(mapping_cryptos, f, indent=4, ensure_ascii=False)
+        except Exception as e:
+            logger.error(f"Erreur lors de l'écriture du fichier: {e}")
+            return None
+            
+        return mapping_cryptos
+
+    except BinanceAPIException as e:
+        logger.error(f"Erreur API Binance: {e.code} - {e.message}")
+        return None
+    except BinanceRequestException as e:
+        logger.error(f"Erreur Request Binance: {e.message}")
+        return None  
+    except Exception as e:
+        logger.error(f"Erreur lors de la récupération des données Binance: {e}")
+        return None
+
+def set_crypto_symbol_to_articles(symbols: list[dict[str, str]] | None = None):
+    if not symbols:
+        symbols = DEFAULT_SYMBOLS
+
+    print(symbols[0])
+
+def main():
+    symbols_and_names=get_cryptos_symbols_and_names()
+    if not symbols_and_names:
+        logger.warning(
+            """La récupération du mapping entre le symbol d'une crypto et son nom n'a pas été possible.
+               Ainsi, seul le Bitcoin sera évalué dans l'identification des symbols présents dans les articles.
+            """)
+
+    set_crypto_symbol_to_articles(symbols_and_names)
+
+
+if __name__ == "__main__":
+    main()
