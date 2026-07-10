61ca17a9ca428a94a8027b53e9af0a00367919c8
Lacorne Ludovic <lacorne.ludovic@gmail.com>
2026-07-10 15:25:16 +0200

    Add monitoring in architecture slide

--- src/visualization/streamlit/slides/architecture.py
+++ src/visualization/streamlit/slides/architecture.py
@@ -13,7 +13,8 @@ with left:
     bullets([
         "<strong>Docker Compose</strong><br/>Les services techniques sont isolés et reproductibles.",
         "<strong>MongoDB & PostgreSQL</strong><br/>MongoDB conserve les données brutes alors que PostgreSQL conserve les données structurées pour l'entrainement, la prédiction, et la visualisation.",
-        "<strong>FastAPI & Streamlit</strong><br/>L’API expose les données et Streamlit rend le projet lisible et démontrable au travers de différents dashboard."
+        "<strong>FastAPI & Streamlit</strong><br/>L’API expose les données et Streamlit rend le projet lisible et démontrable au travers de différents dashboard.",
+        "<strong>Prometheus + Grafana</strong><br/>La supervision suit l’état de la machine, de FastAPI et de Docker afin d’identifier rapidement erreurs, lenteurs ou saturation."
     ])
 with right:
     show_image("services_docker.jpg", "Organisation des principaux services applicatifs.")
