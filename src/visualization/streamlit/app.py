075fd7b95d5ae267cefbbe8d3b908a5ca9953a31
a.ninassi <alexandre.ninassi@calvados.fr>
2026-07-24 11:34:06 +0200

    Modifications page d'accueil

--- src/visualization/streamlit/app.py
+++ src/visualization/streamlit/app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 # Démonstration
 home_page       = st.Page("home.py",        title="Accueil",     icon="💰")
-klines_page     = st.Page("market.py",      title="Marché",      icon="📈")
-sentiment_page  = st.Page("sentiment.py",   title="Sentiment",   icon="📊")
+klines_page     = st.Page("market.py",      title="Marchés",      icon="📈")
+sentiment_page  = st.Page("sentiment.py",   title="Sentiments",   icon="📊")
 labels_page     = st.Page("labels.py",      title="Labels",      icon="🏷️")
 predictions_page= st.Page("predictions.py", title="Prédictions", icon="🎲")
 api_page        = st.Page("api.py",         title="API",         icon="🔌")
